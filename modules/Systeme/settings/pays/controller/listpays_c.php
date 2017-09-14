4a3016d1166a1730a6b20a0f3da275c62ab89578
fatimazahra-hanounou <fatimazahra@dctchad.com>
2017-09-15 00:42:48 +0100

    module_pays

--- modules/Systeme/settings/pays/controller/listpays_c.php
+++ modules/Systeme/settings/pays/controller/listpays_c.php
@@ -1,56 +1,58 @@
-
 <?php
+//SYS MRN ERP
+// Modul: pays => Controller Liste
 	
 	global $db;
 	$params = $columns = $totalRecords = $data = array();
 
 	$params = $_REQUEST;
-	//
 	
 	//define index of column
 	$columns = array( 
-		0 =>'id_departement',
-		1 =>'libelle',
-		2 =>'region',
-		3 =>'statut'
+		0 =>'id_pays',
+		1 =>'libelle', 
+		2 =>'nationalite',
+		3 =>'alpha',
+		4 =>'statut',
 		
 	);
 
 	//Format all variables
 
 	$colms = $tables = $joint = $where = $where_s=$sqlTot = $sqlRec = "";
+
     // define used table.
-	$tables .= " ref_departement, ref_region ";
-    // define joint and table relation
-	$joint .= " AND ref_region.id = ref_departement.id_region";
+	$tables .= " ref_pays ";
+    // define joint and rtable elation
+	$joint .= " ";
 	// set sherched columns.(the final colm without comma)
-	$colms .= " ref_departement.id AS id_departement, ";	
-	$colms .= " ref_departement.departement as libelle, ";
-	$colms .= " ref_region.region as region, ";
+	$colms .= " ref_pays.id AS id_pays, ";	
+	$colms .= " ref_pays.pays as pays, ";
+	$colms .= " ref_pays.nationalite as nationalite, ";
+	$colms .= " ref_pays.alpha as alpha, ";
 
 	//define notif culomn to concatate with any colms.
 	//this is change style of button action to red
-	$notif_colms = TableTools::line_notif_new('ref_departement', 'departements');
-
+	$notif_colms = TableTools::line_notif_new('ref_pays', 'pays');
 	$colms .= $notif_colms;
 
 	//difine if user have rule to show line depend of etat 
-	$where_etat_line = TableTools::where_etat_line('ref_departement', 'departements');
-
+	$where_etat_line = TableTools::where_etat_line('ref_pays', 'pays');
+	
     
-
 	// check search value exist
 	if( !empty($params['search']['value']) or Mreq::tp('id_search') != NULL)  {
 
 		$serch_value = str_replace('+',' ',$params['search']['value']);
-		
         //Format where in case joint isset  
-	    /*$where .= $joint == NULL? " WHERE " : " AND ";*/
+	    $where_s .= $joint == NULL? " WHERE " : " AND ";
 
-		$where_s .="AND ( ref_departement.departement LIKE '%".$serch_value."%' ";
-		$where_s .=" OR ref_region.region '%".$serch_value."%' )";
 
-		$where_s .= TableTools::where_search_etat('ref_departement', 'departements', $serch_value);
+		$where_s .=" (ref_pays.pays LIKE '%".$serch_value."%' ";  
+		$where_s .=" OR ref_pays.nationalite LIKE '%".$serch_value."%' ";
+		$where_s .=" OR ref_pays.alpha LIKE '%".$serch_value."%' )";
+
+		$where_s .= TableTools::where_search_etat('ref_pays', 'pays', $serch_value);
 
 	}
 
@@ -62,7 +64,7 @@
 	$sqlTot .= $sql;
 	$sqlRec .= $sql;
 	//concatenate search sql if value exist
-	if(isset($where) && $where != NULL) {
+	if(isset($where) && $where != '') {
 
 		$sqlTot .= $where;
 		$sqlRec .= $where;
@@ -71,9 +73,11 @@
 	//if we use notification we must ordring lines by nofication rule in first
 	//Change ('notif', status) with ('notif', column where notif code is concated)
 	//on case of order by other parametre this one is disabled 
-    $order_notif = TableTools::order_bloc($params['order'][0]['column']);
+    $order_notif = $params['order'][0]['column'] == 0 ? " CASE WHEN LOCATE('notif', statut) = 0  THEN 0 ELSE 1 END DESC ," : NULL;
 
- 	$sqlRec .=  " ORDER BY $order_notif  ". $columns[$params['order'][0]['column']]."   ".$params['order'][0]['dir']."  LIMIT ".$params['start']." ,".$params['length']." ";
+    
+
+ 	$sqlRec .=  " ORDER BY $order_notif ". $columns[$params['order'][0]['column']]."   ".$params['order'][0]['dir']."  LIMIT ".$params['start']." ,".$params['length']." ";
 
 
     if (!$db->Query($sqlTot)) $db->Kill($db->Error()." SQLTOT $sqlTot");
@@ -84,14 +88,14 @@
     if( Mreq::tp('export')==1 )
     {
     	
-    	$file_name = 'departements_list';
-    	$title     = 'Liste des departements ';
+    	$file_name = 'pays_list';
+    	$title     = 'Liste des pays ';
     	if(Mreq::tp('format')=='csv')
     	{
-    		$header    = array('ID', 'Departement', 'Region','Statut');
+    		$header    = array('ID', 'Pays','Nationalite','Code pays' ,'Statut');
     		Minit::Export_xls($header, $file_name, $title);
     	}else{
-    		$header    = array('ID'=>10, 'Departement'=>45, 'Region'=>35, 'Statut'=>10);
+    		$header    = array('ID'=>10, 'Pays'=>35, 'Nationalite'=>35,'Code pays'=> 10, 'Statut'=>10);
     		Minit::Export_pdf($header, $file_name, $title);
     	}
     	  	
