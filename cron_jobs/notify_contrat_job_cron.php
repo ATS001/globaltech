e3f3812f02476ff8e8443c77556597eab17354ac
fatimazahra-hanounou <fatimazahra@dctchad.com>
2017-10-08 00:02:23 +0100

    updates

--- cron_jobs/notify_contrat_job_cron.php
+++ cron_jobs/notify_contrat_job_cron.php
@@ -7,8 +7,8 @@ $sql = "call notify_contrat_frn();";
 
 if($db->Query($sql))
 {
-   log_cron('Contrat fournisseur notifié', 'notify');
+   log_cron('Contrat fournisseur notifié', 'notify_contrat');
    
 }else{
-	log_cron('Erreur notification', 'notify');
+	log_cron('Erreur notification', 'notify_contrat');
 }
