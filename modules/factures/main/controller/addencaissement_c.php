5ecd08f2aafb88f0ecdfc522f150422acfccd292
fatimazahra-hanounou <fatimazahra@dctchad.com>
2017-10-08 13:38:11 +0100

    okk

--- modules/factures/main/controller/addencaissement_c.php
+++ modules/factures/main/controller/addencaissement_c.php
@@ -1,60 +1,60 @@
 
-    <?php
-   
-    defined('_MEXEC') or die;
-    if (MInit::form_verif('addencaissement', false)) {
+<?php
 
-        $posted_data = array(       
-            'designation' => Mreq::tp('designation'),
-            'idfacture' => Mreq::tp('idfacture'),
-            'montant' => Mreq::tp('montant'),
-            'pj_id'          => Mreq::tp('pj-id'),
-            );
-        
-        
+defined('_MEXEC') or die;
+if (MInit::form_verif('addencaissement', false)) {
+
+    $posted_data = array(       
+        'designation'  => Mreq::tp('designation'),
+        'idfacture'    => Mreq::tp('idfacture'),
+        'montant'      => Mreq::tp('montant'),
+        'pj_id'        => Mreq::tp('pj-id'),
+        );
+
+
         //Check if array have empty element return list
         //for acceptable empty field do not put here
 
-        $checker = null;
-        $empty_list = "Les champs suivants sont obligatoires:\n<ul>";
+    $checker = null;
+    $empty_list = "Les champs suivants sont obligatoires:\n<ul>";
 
-           
-         
-        if($posted_data['designation'] == NULL ){
 
-            $empty_list .= "<li>Désignation</li>";
-            $checker = 1;
-        }
-        
-        if($posted_data['idfacture'] == NULL){
 
-            $empty_list .= "<li>Facture</li>";
-            $checker = 1;
-        }
-        
-        if ($posted_data['montant'] == NULL) {
+    if($posted_data['designation'] == NULL ){
 
-            $empty_list .= "<li>Montant</li>";
-            $checker = 1;
-        }
-                   
-      
-        $empty_list .= "</ul>";
-        if ($checker == 1) {
-           exit("0#$empty_list");
-        }
-       
-        $new_encaissement = new Mfacture($posted_data);
-        $new_encaissement->exige_pj=false;
-        
-        //execute Insert returne false if error
-        if ($new_encaissement->save_new_encaissement()) {
-
-            echo("1#" . $new_encaissement->log);
-        } else {
-
-            echo("0#" . $new_encaissement->log);
-        }
-    } else {
-        view::load_view('addencaissement');
+        $empty_list .= "<li>Désignation</li>";
+        $checker = 1;
     }
+
+    if($posted_data['idfacture'] == NULL){
+
+        $empty_list .= "<li>Facture</li>";
+        $checker = 1;
+    }
+
+    if ($posted_data['montant'] == NULL) {
+
+        $empty_list .= "<li>Montant</li>";
+        $checker = 1;
+    }
+
+
+    $empty_list .= "</ul>";
+    if ($checker == 1) {
+     exit("0#$empty_list");
+ }
+
+ $new_encaissement = new Mfacture($posted_data);
+ $new_encaissement->exige_pj=false;
+
+        //execute Insert returne false if error
+ if ($new_encaissement->save_new_encaissement()) {
+
+    echo("1#" . $new_encaissement->log);
+} else {
+
+    echo("0#" . $new_encaissement->log);
+}
+} else {
+    view::load_view('addencaissement');
+}
