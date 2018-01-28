0e378730a175b7a2c826a0c89e95d2a325e06c76
fatimazahra-hanounou <fatimazahra@dctchad.com>
2018-01-28 20:55:51 +0000

    last modif contrat

---
+++ modules/vente/submodul/contrats/controller/resiliercontrat_c.php
@@ -0,0 +1,24 @@
+
+<?php 
+	//SYS GLOBAL TECH
+	// Modul: contrats => Controller
+
+	//Get contrat info
+	$contrat = new Mcontrat();
+	$contrat->id_contrat = Mreq::tp('id');
+
+
+	if(!MInit::crypt_tp('id', null, 'D')or !$contrat->get_contrat())
+	{  
+	   // returne message error red to contrat 
+	   exit('0#<br>Les informations pour cette ligne sont erronées contactez l\'administrateur');
+	}
+
+
+	if($contrat->valid_contrats(5))
+	{
+		exit("1#".$contrat->log);
+
+	}else{
+		exit("0#".$contrat->log);
+	}
