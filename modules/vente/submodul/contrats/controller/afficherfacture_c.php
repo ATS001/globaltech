28d115e8a4c576e55a25293c6d3cd8d2c70480b4
ayoubdct <ayoub@dctchad.com>
2018-03-31 12:49:00 +0100

    big del

--- modules/vente/submodul/contrats/controller/afficherfacture_c.php
+++
@@ -1,8 +0,0 @@
-<?php 
-//First check target no Hack
-if(!defined('_MEXEC'))die();
-//SYS GLOBAL TECH
-// Modul: contrats
-//Created : 03-03-2018
-//Controller PROFILE Form
-view::load_view('afficherfacture');
--- modules/vente/submodul/contrats/controller/echeances_c.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//First check target no Hack
-if(!defined('_MEXEC'))die();
-//SYS GLOBAL TECH
-// Modul: contrats
-//Created : 26-02-2018
-//Controller
-//Call view of task 
-view::load_view('echeances');
