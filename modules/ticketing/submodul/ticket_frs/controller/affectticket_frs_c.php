d423af0004de7364012701fbf5487915ce6aa340
ayoubdct <ayoub@dctchad.com>
2018-07-18 21:34:24 +0100

    Commit

--- modules/ticketing/submodul/ticket_frs/controller/affectticket_frs_c.php
+++ modules/ticketing/submodul/ticket_frs/controller/affectticket_frs_c.php
@@ -1,110 +1,60 @@
-<?php 
+<?php
+
 //First check target no Hack
-if(!defined('_MEXEC'))die();
+if (!defined('_MEXEC'))
+{die();}
 //SYS GLOBAL TECH
-// Modul: ticket_frs
-//Created : 15-07-2018
+// Modul: tickets
+//Created : 03-04-2018
 //Controller ADD Form
-if(MInit::form_verif('affectticket_frs', false))
-{
+if (MInit::form_verif('affecttechnicien', false)) {
 
-		$posted_data = array(
+    $posted_data = array(
+        'id' => Mreq::tp('id'),
+        'id_technicien' => Mreq::tp('id_technicien'),
+    );
+    
+    //Check if array have empty element return list
+    //for acceptable empty field do not put here
+    $checker = null;
+    $empty_list = "Les champs suivants sont obligatoires:\n<ul>";
 
-			'id_fournisseur'         => Mreq::tp('id_fournisseur') ,
-'date_incident'         => Mreq::tp('date_incident') ,
-'nature_incident'         => Mreq::tp('nature_incident') ,
-'description'         => Mreq::tp('description') ,
-'prise_charge_frs'         => Mreq::tp('prise_charge_frs') ,
-'prise_charge_glbt'         => Mreq::tp('prise_charge_glbt') ,
-'id_technicien'         => Mreq::tp('id_technicien') ,
-'date_affectation'         => Mreq::tp('date_affectation') ,
-'code_cloture'         => Mreq::tp('code_cloture') ,
-'observation'         => Mreq::tp('observation') ,
+    
+    if ($posted_data["id_technicien"] == NULL) {
+        $empty_list .= "<li>Technicien</li>";
+        $checker = 1;
+    }
+  
 
+    $empty_list .= "</ul>";
+    if ($checker == 1) {
+        exit("0#$empty_list");
+    }
 
-		);
+    //End check empty element
+    $new_tickets = new Mtickets($posted_data);
+    $new_tickets->id_tickets=$posted_data["id"];
+   
+        
+//Etat for validate row
+$etat = Msetting::get_set('etat_ticket', 'resolution_encours');
 
+    //execute Insert returne false if error
+    if ($new_tickets->affect_ticket($is_reaffect=FALSE)) {
+        if($new_tickets->valid_tickets($etat)){
+            exit("1#" . $new_tickets->log);
+        } else {
+            exit("0#" . $new_tickets->log);
+        }
 
-        //Check if array have empty element return list
-        //for acceptable empty field do not put here
-		$checker = null;
-		$empty_list = "Les champs suivants sont obligatoires:\n<ul>";
+        exit("1#" . $new_tickets->log);
+    } else {
 
-			if($posted_data["id_fournisseur"] == NULL){
-                                    $empty_list .= "<li>Fournisseur</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["date_incident"] == NULL){
-                                    $empty_list .= "<li>Date incident</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["nature_incident"] == NULL){
-                                    $empty_list .= "<li>Nature incident</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["description"] == NULL){
-                                    $empty_list .= "<li>Description</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["prise_charge_frs"] == NULL){
-                                    $empty_list .= "<li>Prise en charge Fournisseur</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["prise_charge_glbt"] == NULL){
-                                    $empty_list .= "<li>Prise en charge Globaltech</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["id_technicien"] == NULL){
-                                    $empty_list .= "<li>Technicien</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["date_affectation"] == NULL){
-                                    $empty_list .= "<li>Date affectation</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["code_cloture"] == NULL){
-                                    $empty_list .= "<li>Code clôture</li>";
-                                    $checker = 1;
-                              }
-	if($posted_data["observation"] == NULL){
-                                    $empty_list .= "<li>Observation</li>";
-                                    $checker = 1;
-                              }
-
-
-
-		$empty_list.= "</ul>";
-		if($checker == 1)
-		{
-			exit("0#$empty_list");
-		}
-
-
-
-       //End check empty element
-		$new_ticket_frs = new  Mticket_frs($posted_data);
-
-
-
-        //execute Insert returne false if error
-		if($new_ticket_frs->save_new_ticket_frs()){
-
-			exit("1#".$new_ticket_frs->log);
-		}else{
-
-			exit("0#".$new_ticket_frs->log);
-		}
-
-
+        exit("0#" . $new_tickets->log);
+    }
+       
 }
 
 //No form posted show view
-view::load_view('affectticket_frs');
-
-
-
-
-
-
-
-	?>
+view::load_view('affecttechnicien');
+?>
--- modules/ticketing/submodul/ticket_frs/view/affectticket_frs_v.php
+++ modules/ticketing/submodul/ticket_frs/view/affectticket_frs_v.php
@@ -1,113 +1,171 @@
-<?php 
+<?php
 //First check target no Hack
-if(!defined('_MEXEC'))die();
-//SYS GLOBAL TECH
-// Modul: ticket_frs
-//Created : 15-07-2018
-//View
+if (!defined('_MEXEC'))
+    die();
+
+$ticket = new Mticket_frs;
+//Set ID of Module with POST id
+$ticket->id_tickets = Mreq::tp('id');
+//$ticket->get_tickets();
+//Check if Post ID <==> Post idc or get_modul return false. 
+if (!MInit::crypt_tp('id', null, 'D') or ! $ticket->get_ticket_frs()) {
+    // returne message error red to client 
+    exit('3#' . $ticket->log . '<br>Les informations pour cette ligne sont erronées contactez l\'administrateur');
+}
 ?>
+
 <div class="pull-right tableTools-container">
-	<div class="btn-group btn-overlap">
-				
-		<?php TableTools::btn_add('ticket_frs','Liste des ticket_frs', Null, $exec = NULL, 'reply'); ?>
-					
-	</div>
+    <div class="btn-group btn-overlap">
+
+        <?php TableTools::btn_add('ticket_frs', 'Liste des tickets', Null, $exec = NULL, 'reply'); ?>
+
+    </div>
 </div>
 <div class="page-header">
-	<h1>
-		Ajouter un ticket_frs
-		<small>
-			<i class="ace-icon fa fa-angle-double-right"></i>
-		</small>
-	</h1>
+    <h1>
+        Affecter un ticket
+        <small>
+            <i class="ace-icon fa fa-angle-double-right"></i>
+        </small>
+    </h1>
 </div><!-- /.page-header -->
+
+
+<?php
+$form = new Mform('affectticket_frs', 'affectticket_frs', '', 'ticket_frs', '0', null);
+$form->input_hidden('id', $ticket->g('id'));
+$form->input_hidden('idc', Mreq::tp('idc'));
+$form->input_hidden('idh', Mreq::tp('idh'));
+
+//Technicien ==> 
+$array_technicien[] = array("required", "true", "Choisir un technicien");
+$form->select_table('Technicien', 'id_technicien', 6, 'users_sys', 'id', 'id', 'CONCAT(users_sys.lnom," ",users_sys.fnom)', $indx = '------', $selected = NULL, $multi = NULL, $where = ' service=6 AND etat=1', $array_technicien, NULL);
+
+//var_dump($info_tickets);
+?>
 <!-- Bloc form Add Devis-->
 <div class="row">
-	<div class="col-xs-12">
-		<div class="clearfix">
-			
-		</div>
-		<div class="table-header">
-			Formulaire: "<?php echo ACTIV_APP; ?>"
-		</div>
-		<div class="widget-content">
-			<div class="widget-box">
-				
-<?php
- 
-$form = new Mform('affectticket_frs', 'affectticket_frs', '', 'ticket_frs', '0', null);
+    <div class="col-xs-12">
+        <div class="clearfix">
 
-//Date Example
-//$array_date[]= array('required', 'true', 'Insérer la date de ...');
-//$form->input_date('Date', 'date_', 4, date('d-m-Y'), $array_date);
-//Select Table Example
-
-
-//$select_array[]  = array('required', 'true', 'Choisir un ....');
-//$form->select_table('Select ', 'select', 8, 'table', 'id', 'text' , 'text', $indx = '------' ,$selected=NULL,$multi=NULL, $where=NULL, $select_array, null);
+        </div>
+        <div class="table-header">
+            Formulaire: "<?php echo ACTIV_APP; ?>"
+        </div>
+        <div class="widget-content">
+            <div class="widget-box">
 
 
 
-//Select Simple Example
-//$field_opt = array('O' => 'OUI' , 'N' => 'NON' );
-//$form->select('Label Field', 'field', 2, $field_opt, $indx = NULL ,$selected = NULL, $multi = NULL);
-
-//Separate Zone title
-//$form->bloc_title('Zone separated');
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div>
+                            <div id="user-profile-2" class="user-profile">
 
 
-//Input Example
-//$form->input('Label field', 'field', 'text' ,'class', '0', null, null, $readonly = null);
-//For more Example see form class
+                                <div class="tab-content no-border padding-24">
+                                    <div id="home" class="tab-pane in active">
+                                        <div class="row">
+                                            <div class="col-sm-4">
 
-//Fournisseur ==> 
-	$array_id_fournisseur[]= array("required", "true", "Insérer Fournisseur ...");
-	$form->input("Fournisseur", "id_fournisseur", "text" ,"9", null, $array_id_fournisseur, null, $readonly = null);
-//Date incident ==> 
-	$array_date_incident[]= array("required", "true", "Insérer Date incident ...");
-	$form->input("Date incident", "date_incident", "text" ,"9", null, $array_date_incident, null, $readonly = null);
-//Nature incident ==> 
-	$array_nature_incident[]= array("required", "true", "Insérer Nature incident ...");
-	$form->input("Nature incident", "nature_incident", "text" ,"9", null, $array_nature_incident, null, $readonly = null);
-//Description ==> 
-	$array_description[]= array("required", "true", "Insérer Description ...");
-	$form->input("Description", "description", "text" ,"9", null, $array_description, null, $readonly = null);
-//Prise en charge Fournisseur ==> 
-	$array_prise_charge_frs[]= array("required", "true", "Insérer Prise en charge Fournisseur ...");
-	$form->input("Prise en charge Fournisseur", "prise_charge_frs", "text" ,"9", null, $array_prise_charge_frs, null, $readonly = null);
-//Prise en charge Globaltech ==> 
-	$array_prise_charge_glbt[]= array("required", "true", "Insérer Prise en charge Globaltech ...");
-	$form->input("Prise en charge Globaltech", "prise_charge_glbt", "text" ,"9", null, $array_prise_charge_glbt, null, $readonly = null);
-//Technicien ==> 
-	$array_id_technicien[]= array("required", "true", "Insérer Technicien ...");
-	$form->input("Technicien", "id_technicien", "text" ,"9", null, $array_id_technicien, null, $readonly = null);
-//Date affectation ==> 
-	$array_date_affectation[]= array("required", "true", "Insérer Date affectation ...");
-	$form->input("Date affectation", "date_affectation", "text" ,"9", null, $array_date_affectation, null, $readonly = null);
-//Code clôture ==> 
-	$array_code_cloture[]= array("required", "true", "Insérer Code clôture ...");
-	$form->input("Code clôture", "code_cloture", "text" ,"9", null, $array_code_cloture, null, $readonly = null);
-//Observation ==> 
-	$array_observation[]= array("required", "true", "Insérer Observation ...");
-	$form->input("Observation", "observation", "text" ,"9", null, $array_observation, null, $readonly = null);
+                                                <div>
+                                                    <ul class="list-unstyled spaced">
+                                                        <?php if ($ticket->g("technicien") != NULL) { ?>
+                                                            <li>
+                                                                <i class="ace-icon fa fa-caret-right green"></i>Technicien précèdent :
+                                                                <b style="color:green"><?php $ticket->s("technicien") ?></b>
+                                                            </li> 
+                                                            <li>
+                                                                <i class="ace-icon fa fa-caret-right green"></i>Date affectation :
+                                                                <b style="color:green"><?php $ticket->s("date_affectation") ?></b>
+                                                            </li>
+                                                            <li>
+                                                                <i class="ace-icon fa fa-caret-right green"></i>Nombre de jour :
+                                                                <b style="color:green"><?php $ticket->s("nbrj") ?></b>
+                                                            </li> 
+                                                        <?php } ?>
+                                                        <li>
+                                                            <i class="ace-icon fa fa-caret-right green"></i>Fournisseur :
+                                                            <b style="color:green"><?php $ticket->s("fournisseur") ?></b>
+                                                        </li>
+                                                       
+                                                        <li>
+                                                            <i class="ace-icon fa fa-caret-right green"></i>Type produit:
+                                                            <b style="color:green"><?php $ticket->s("typep") ?></b>
+                                                        </li>
+                                                        <li>
+                                                            <i class="ace-icon fa fa-caret-right green"></i>Catégorie produit :
+                                                            <b style="color:green"><?php $ticket->s("categorie_produit") ?></b>
+                                                        </li>
+                                                        <?php if ($ticket->g("prd") != NULL) { ?>
+                                                            <li>
+                                                                <i class="ace-icon fa fa-caret-right green"></i>Produit :
+                                                                <b style="color:green"><?php $ticket->s("prd") ?></b>
+                                                            </li>
+                                                        <?php } ?>
+                                                        <li>
+                                                            <i class="ace-icon fa fa-caret-right green"></i>Date prévisionnelle :
+                                                            <b style="color:green"><?php $ticket->s("date_previs") ?></b>
+                                                        </li>
+                                                    </ul>
+
+                                                </div>
+
+                                            </div><!-- /.col -->
 
 
-$form->button('Enregistrer');
+                                            <div class="col-sm-8">
+                                                <div>
+                                                    <div class="space-6"></div>
+                                                    <div class="sa">
+                                                        <?php $ticket->s("message") ?>
+                                                    </div>
+                                                </div>
+                                            </div><!-- /.col -->
+                                        </div>
+
+                                    </div><!-- /#home -->
+
+
+                                </div><!-- /.row -->
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <?php
+            $form->button('Enregistrer');
 //Form render
-$form->render();
-?>
-			</div>
-		</div>
-	</div>
+            $form->render();
+            ?>
+        </div>
+    </div>
+</div>
 </div>
 <!-- End Add devis bloc -->
-		
-<script type="text/javascript">
-$(document).ready(function() {
-    
-//JS bloc   
 
-});
+<script type="text/javascript">
+    $(document).ready(function () {
+
+
+    });
 </script>	
 
-		
+<style>
+    .sa {
+        min-height: 20px;
+        padding: 19px;
+        margin-bottom: 20px;
+        background-color: #DEE4EA;
+        border: 2px solid #e3e3e3;
+        border-radius: 4px;
+        -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);
+    }
+
+</style>
