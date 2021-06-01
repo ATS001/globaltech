b3de5e9ff75df960196877f7e570fc9f6306ee6a
fatimazahra-hanounou <fatimazahra@dctchad.com>
2021-06-01 19:15:54 +0100

    Modifiations du 01/06 pour permission tableau de bord

--- modules/tdb/view/indicators_v_old.php
+++ modules/tdb/view/indicators_v_old.php
@@ -2,13 +2,10 @@
   <div class="col-xs-12">        <!-- Small boxes (Stat box) -->
     <div class="row">
       <?php 
-      
-      if(session::get('service') == 3 OR session::get('service') == 1 OR session::get('service') == 7 OR session::get('service') == 2){
         Mobjectif_mensuel::indicator_objectif_mensuel();
         Mobjectif_service::indicator_objectif_service();
         Mdevis::indicator_nbr_devis(); 
         Mdevis::indicator_nbr_facture_non_paye();
-      }
       ?> 
     </div>
   </div>
